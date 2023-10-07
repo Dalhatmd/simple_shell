f9445f8cad42c42c4ff80bf5d5ebdb445b68b080
Dalhatmd <umarwalid4055@gmail.com>
2023-10-07 17:21:08 +0000

    Started working on first task

--- main.c
+++ main.c
@@ -1,24 +1,51 @@
 #include "shell.h"
-int main()
+int main(void)
 {
 	char *command = NULL;
+	char *envp[] = {NULL};
 	size_t len = 0;
-	ssize_t read;
+	ssize_t bytes_read;
+	int status;
+	pid_t pid;
 
 	while (1)
 	{
 		printf("$ ");
-		read = getline(&command, &len, stdin);
-		if (read == -1)
+		bytes_read = getline(&command, &len, stdin);
+		if (bytes_read == -1)
 		{
-			exit(0);
+			printf("\n");
+			exit(1);
+		}
+		if (bytes_read > 0 && command[bytes_read - 1] == '\n')
+		{
+			command[bytes_read - 1] = '\0';
+		}
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("Fork failed\n");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			char *argv[] = {command, NULL};
+			execve(command, argv, envp);
 		}
 		else
 		{
-			execute(command);
-			printf("%s\n", command);
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+			{
+				printf("child exited with status%d\n", WEXITSTATUS(status));
+			}
+			else if(WIFSIGNALED(status))
+			{
+				printf("Child terminated with signal %d\n", WTERMSIG(status));
+			}
+
 		}
 	}
 	free(command);
-	return 0;
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -5,8 +5,10 @@ extern char **environ;
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 void execute(char *command);
 
 #endif
